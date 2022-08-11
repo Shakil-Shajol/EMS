3cfe77912ec91d2aab8b9a7b454c0beaa2743d0b
Shakil Ahmmed Shajol <shakil.bd.ngn@gmail.com>
2022-08-12 04:24:08 +0600

    Client Project Added

---
+++ EMS.Client/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using EMS.Client.Helpers;
+using EMS.Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EMS.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly HttpCallHandler _httpCallHandler;
+
+        public HomeController(ILogger<HomeController> logger,HttpCallHandler httpCallHandler)
+        {
+            _logger = logger;
+            this._httpCallHandler = httpCallHandler;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var employee = new { EmployeeName = "Azman Mollah", DepartmentId = 2, JoinDate = "18-12-1993" };
+            var res = await _httpCallHandler.PostAsync("api/employee", employee);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EMS.Client/Helpers/HttpCallHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Http.Headers;
+
+namespace EMS.Client.Helpers
+{
+    public class HttpCallHandler
+    {
+        private readonly IConfiguration configuration;
+
+        public HttpCallHandler(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task<string> GetValue(string apiUrl)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(configuration.GetValue<string>("ApiBaseUrl"));
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                return result;
+            }
+            else
+            {
+                return "{}";
+            }
+        }
+
+        public async Task<string> PostAsync(string apiUrl,object obj)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(configuration.GetValue<string>("ApiBaseUrl"));
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await client.PostAsJsonAsync(apiUrl, obj);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                return result;
+            }
+            else
+            {
+                return "{}";
+            }
+        }
+    }
+}
